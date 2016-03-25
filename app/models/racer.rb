f9d49688fa463ba2d8804aeef1e951c982bf795d
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-25 23:00:36 +0000

    racers_spec 03

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,8 @@
 class Racer
   include Mongoid::Document
+  embeds_one :info, as: :parent, class_name: 'RacerInfo', autobuild: true
+  
+  before_create do |racer|
+    racer.info.id = racer.id
+  end
 end
--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -2,9 +2,10 @@ class RacerInfo
   include Mongoid::Document
   field :racer_id, as: :_id
   field :_id, default:->{ racer_id }
-  field :first_name, type: String, as: :fn
-  field :last_name, type: String, as: :ln
-  field :gender, type: String, as: :g
-  field :birth_year, type: Integer, as: :yr
-  field :residence, type: Address, as: :res
+  field :fn, type: String, as: :first_name
+  field :ln, type: String, as: :last_name
+  field :g, type: String, as: :gender
+  field :yr, type: Integer, as: :birth_year
+  field :res, type: Address, as: :residence
+  embedded_in :parent, polymorphic: true
 end
