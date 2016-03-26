8f295a7ceafcd493dc40bd5cbf110a92eaee2e35
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-26 01:06:52 +0000

    races_spec 02

---
+++ app/models/event.rb
@@ -0,0 +1,32 @@
+class Event
+  include Mongoid::Document
+  field :o, type: Integer, as: :order
+  field :n, type: String, as: :name
+  field :d, type: Float, as: :distance
+  field :u, type: String, as: :units
+  embedded_in :event, polymorphic:true
+
+  def meters
+    result=0
+    if units=="meters"
+        result =distance
+    elsif units=="kilometers"
+        result =distance*1000
+    elsif units=="miles"
+        result = distance/0.000621371
+    elsif units=="yards"
+        results = distance*0.9144
+    else
+        nil
+    end
+  end
+
+  def miles
+    if meters
+        meters/1609.344
+    else
+        nil
+    end
+  end
+
+end
