8354d4614fe73df32392489bb4978b5ea74464d5
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-26 00:42:52 +0000

    races_spec 01

---
+++ app/models/race.rb
@@ -0,0 +1,8 @@
+class Race
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :n, type: String, as: :name
+  field :date, type: Date
+  field :loc, type: Address, as: :location
+end
