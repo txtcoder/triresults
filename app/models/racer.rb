b8918180cb1f51a5cf5cfddd4d0e9689351a1172
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-29 13:30:41 +0000

    racer_results

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,7 +1,8 @@
 class Racer
   include Mongoid::Document
   embeds_one :info, as: :parent, class_name: 'RacerInfo', autobuild: true
-  
+  has_many :races, class_name: "Entrant", foreign_key: "racer.racer_id", dependent: :nullify, order: :"race.date".desc
+
   before_create do |racer|
     racer.info.id = racer.id
   end
