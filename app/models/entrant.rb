2a2f39f34882a4efef192588b2d997e43e69acca
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-29 13:26:28 +0000

    racer_result 01

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -12,6 +12,7 @@ class Entrant
   embeds_many :results, as: :entrant, class_name: "LegResult", after_add: :update_total, after_remove: :update_total
 
   embeds_one :race, class_name: "RaceRef"
+  embeds_one :racer, as: :parent, class_name: "RacerInfo"
   def update_total(result)
     self.secs=results.inject(0){|s,x| s+x.secs}
   end
