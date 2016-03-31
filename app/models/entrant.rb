07ebed9c515ad55cd774a191e475fafdcefb570f
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-31 01:09:04 +0000

    flatten_attributes 03

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -11,8 +11,36 @@ class Entrant
 
   embeds_many :results, as: :entrant, class_name: "LegResult", after_add: :update_total, after_remove: :update_total
 
-  embeds_one :race, class_name: "RaceRef"
-  embeds_one :racer, as: :parent, class_name: "RacerInfo"
+  embeds_one :race, class_name: "RaceRef", autobuild: true
+  embeds_one :racer, as: :parent, class_name: "RacerInfo", autobuild: true
+
+  delegate :first_name, :first_name=, to: :racer
+  delegate :last_name, :last_name=, to: :racer
+  delegate :gender, :gender=, to: :racer, prefix: "racer"
+  delegate :birth_year, :birth_year=, to: :racer
+  delegate :city, :city=, to: :racer
+  delegate :state, :state=, to: :racer
+
+  delegate :name, :name=, to: :race, prefix: "race"
+  delegate :date, :date=, to: :race, prefix: "race"
+
+
+  def overall_place
+    overall.place if overall
+  end
+
+  def gender_place
+    gender.place if gender
+  end
+
+  def group_name
+    group.name if group
+  end
+
+  def group_place
+    group.place if group
+  end
+
   def update_total(result)
     self.secs=results.inject(0){|s,x| s+x.secs}
   end
