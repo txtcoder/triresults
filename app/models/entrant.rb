8ef16e4181fcda53d5f41d43c0a459b7362c83e6
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-03-31 01:31:40 +0000

    flatten_attributes 04

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -42,10 +42,43 @@ class Entrant
   end
 
   def update_total(result)
-    self.secs=results.inject(0){|s,x| s+x.secs}
+    self.secs=results.inject(0){|s,x| if x.secs then s+x.secs else s end}
   end
 
   def the_race
     race.race
   end
+
+  RESULTS = {"swim"=>SwimResult,
+    "t1"=>LegResult,
+    "bike"=>BikeResult,
+    "t2"=>LegResult,
+    "run"=>RunResult}
+
+  RESULTS.keys.each do |name|
+    define_method("#{name}") do
+      result=results.select{|result| name==result.event.name if result.event}.first
+      if !result
+        result=RESULTS["#{name}"].new(:event=>{:name=>name})
+        results << result
+      end
+      result
+    end
+
+    define_method("#{name}=") do |event|
+        event=self.send("#{name}").build_event(event.attributes)
+    end
+
+    RESULTS["#{name}"].attribute_names.reject {|r|/^_/===r}.each do |prop|
+        define_method("#{name}_#{prop}") do
+            event=self.send(name).send(prop)
+        end
+        define_method("#{name}_#{prop}=") do |value|
+            event=self.send(name).send("#{prop}=",value)
+            update_total nil if /secs/===prop
+        end
+    end
+  end
 end
+
+
