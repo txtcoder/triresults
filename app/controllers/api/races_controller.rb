423e5a3b44058a28dabdb37582bedae22efaf149
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-04-20 02:58:23 +0000

    uri 03

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -2,9 +2,17 @@ module Api
   class RacesController < ApplicationController
 
     def index
-    end
+        if !request.accept || request.accept == "*/*"
+            render plain: "/api/races"
+        else
+        end
+     end
 
     def show
+        if !request.accept || request.accept == "*/*"
+            render plain: "/api/races/#{params[:id]}"
+        else
+        end
     end
   end
 end
